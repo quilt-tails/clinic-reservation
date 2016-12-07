cb1ea37cdaf7866d7a2ccd6e4be5d1357d1e3978
quilt-tails <siriuscanopusmg@yahoo.co.jp>
2016-12-07 18:32:50 +0000

    Add an Application Helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "わきあいアイクリニック"
+  end
+  
   test "should get index" do
     get :index
     assert_response :success
-    assert_select "title", "Home | わきあいアイクリニック"
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,5 +5,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/index'
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
+  
 end
